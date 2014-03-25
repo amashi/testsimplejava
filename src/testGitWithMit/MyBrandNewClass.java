c4bf1a27a435d738e993d0d0213586d16d84a2aa
shdmitry2000 <shdmitry@gmail.com>
2014-03-25 17:11:33 +0200

    terwrewsf

--- src/testGitWithMit/MyBrandNewClass.java
+++ src/testGitWithMit/MyBrandNewClass.java
@@ -5,6 +5,8 @@ public class MyBrandNewClass {
 	public static void main(String[] args) {
 		System.out.println("hello i'm dimiiiiiii");
 		
+		System.out.println("hello i'm dimiiiiiii");
+		
 
 	}
 
