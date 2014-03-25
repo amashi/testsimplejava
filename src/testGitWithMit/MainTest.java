0eec7ec626a34ba65972f75b255e78f1fad9f6f0
amashi <erez.vaxman@gmail.com>
2014-03-25 15:40:16 +0200

    dasfasdf

--- src/testGitWithMit/MainTest.java
+++ src/testGitWithMit/MainTest.java
@@ -5,6 +5,12 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("test git mit");
 		
+		testMe();
+		
+	}
+	private static void testMe() {
+		System.out.println("ravvvv");
+		
 	}
 	public MainTest() {
 		
